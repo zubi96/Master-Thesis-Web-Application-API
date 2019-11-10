b8e6471a1fd4aacd74b7a18814923f24317817e0
Ivan Zubak <ivanzubizubak@gmail.com>
2019-11-10 21:50:05 +0100

    super-admin initial CRUD

---
+++ Dtos/ModeratorForCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterThesisWebApplication.Dtos
+{
+    public class ModeratorForCreationDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int RegionId { get; set; }
+    }
+}
---
+++ Models/Region.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterThesisWebApplication.Models
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; }
+        public virtual ICollection<Admin> Admins { get; set; }
+    }
+}
